dc25e37b4b0ea04afa35d2b67b33f388a6d0aee4
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-12-28 16:45:09 -0500

    move conf out to conf.json

---
+++ README.md
@@ -0,0 +1,7 @@
+# tim-proxy
+
+## Usage
+
+```bash
+./proxy.js path/to/ssl/credentials/ # if unspecified, defaults to /etc/letsencrypt/live/tradle.io/
+```
--- proxy.js
+++ proxy.js
@@ -6,39 +6,46 @@ var https = require('https')
 var debug = require('debug')('proxy')
 var httpProxy = require('http-proxy')
 var HttpProxyRules = require('http-proxy-rules')
-var credentialsPath = process.argv[2] || '/etc/letsencrypt/live/tradle.io/'
-try {
-  var privateKey = fs.readFileSync(path.join(credentialsPath, 'privkey.pem'))
-  var certificate = fs.readFileSync(path.join(credentialsPath, 'cert.pem'))
-  var credentials = {
-    key: privateKey,
-    cert: certificate
-  }
-} catch (err) {
-  console.log('unable to read TLS certificate, running in HTTP mode')
-}
+var confPath = process.argv[2]
+if (!confPath) throw new Error('specify path/to/conf.json')
 
-// Set up proxy rules instance
-var DEFAULT_TARGET = 'http://localhost:8080'
+var conf = require(path.resolve(confPath))
+var proxy = httpProxy.createProxy()
+// Set up proxy
 var proxyRules = new HttpProxyRules({
-  rules: {
-    '^/keeper': 'http://localhost:25667/',
-    '^/bank': 'http://localhost:44444/'
-  }
+  rules: conf.rules
 })
 
-var proxy = httpProxy.createProxy()
 proxy.on('error', function (err, req, res) {
   debug('error handling request', req.url, err)
   res.writeHead(500, { 'Content-Type': 'text/plain' });
   res.end('internal error');
 })
 
+var protocol
+var conf = require(path.resolve(process.cwd(), confPath))
+var credentials
 var server
 var port
+var defaultTarget
+var credentialsPath = conf.sslCredentialsPath
+if (credentialsPath) {
+  try {
+    var privateKey = fs.readFileSync(path.join(credentialsPath, 'privkey.pem'))
+    var certificate = fs.readFileSync(path.join(credentialsPath, 'cert.pem'))
+    credentials = {
+      key: privateKey,
+      cert: certificate
+    }
+  } catch (err) {
+    console.log('unable to read TLS certificate, running in HTTP mode')
+  }
+}
+
 if (credentials) {
   server = https.createServer(credentials, requestHandler)
   port = 443
+  defaultTarget = 'http://localhost:80'
 } else {
   server = http.createServer(requestHandler)
   port = 80
@@ -47,8 +54,13 @@ if (credentials) {
 server.listen(port)
 
 function requestHandler (req, res) {
-  var target = proxyRules.match(req) || DEFAULT_TARGET
-  return proxy.web(req, res, {
-    target: target
-  })
+  var target = proxyRules.match(req) || defaultTarget
+  if (target) {
+    return proxy.web(req, res, {
+      target: target
+    })
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Is this the error page you were looking for?');
 }
